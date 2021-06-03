4d2db6161f2cbaa8f75cdade2f35d119620c0d19
skabdullah9 <skabdullah9941@gmail.com>
2021-06-03 20:34:29 +0530

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,101 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper" :class="[this.class]">
+    <div class="container">
+      <searchForm @bgClass="this.setClass" />
+      
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import searchForm from "./components/searchForm.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    searchForm,
+  },
+  data() {
+    return {
+      class: ''
+    }
+  },
+  methods: {
+    setClass(gif) {
+      this.class = gif;
+      
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+
+body {
+  font-family: cursive;
+  margin: 0;
+  overflow: hidden;
+}
+.wrapper {
+  background-color: #333;
+  color: #fff;
+  height: 100vh;
+}
+.wrapper.cloudy,
+.wrapper.overcast,
+.wrapper.clouds {
+  background:linear-gradient(45deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(./assets/cloudy.gif) no-repeat;
+}
+.wrapper.haze {
+   background: linear-gradient(45deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(./assets/haze.gif) no-repeat;
+}
+.wrapper.sunny,
+.wrapper.clear {
+    background: linear-gradient(45deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(./assets/sunny.gif) no-repeat;
+}
+.wrapper.rain,
+.wrapper.rainy,
+.wrapper.shower {
+   background: linear-gradient(45deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(./assets/rain.gif) no-repeat;
+}
+.wrapper.mist,
+.wrapper.foggy {
+  background: linear-gradient(45deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(./assets/fog.gif) no-repeat;
+}
+.wrapper.thunder,
+.wrapper.storm {
+  background: linear-gradient(45deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(./assets/thunder.gif) no-repeat;
+}
+.wrapper.sky {
+  background: linear-gradient(45deg, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(./assets/sun.gif) no-repeat;
+}
+
+.wrapper.haze,
+.wrapper.cloudy,
+.wrapper.clouds,
+.wrapper.sunny,
+.wrapper.clear,
+.wrapper.rain,
+.wrapper.rainy,
+.wrapper.shower,
+.wrapper.overcast,
+.wrapper.mist,
+.wrapper.foggy,
+.wrapper.thunder,
+.wrapper.storm,
+.wrapper.sky  {
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+  max-height: 100%;
+}
+
+.container {
+  max-width: 1140px;
+  width: 95%;
+  margin: 0 auto;
+  box-sizing: border-box;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding-top: 5rem;
 }
 </style>
